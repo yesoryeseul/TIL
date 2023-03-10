7f4f2e036afe125f8a99418b1d7d50b5f58642b0
yesoryeseul <yesoryeseul@gmail.com>
2023-03-10 22:05:30 +0900

    코드페스티벌 js 문제 풀이
    37~40번 문제 난도 up

--- 코드페스티벌/js/ch01_31_40.js
+++ 코드페스티벌/js/ch01_31_40.js
@@ -56,13 +56,37 @@ console.log(res);
 // 입력 : 155 156 165 166 169 176
 // 출력 : YES
 
-let heightIn = prompt("공백으로 6명의 키를 입력하세요").split(" ");
+let heightIn = prompt("공백으로 6명의 키를 입력하세요");
+let sorted = "";
 
-// if (heightIn.sort()) {
-//   console.log("YES");
-// } else {
-//   console.log("NO");
-// }
+sorted = heightIn
+  .split(" ")
+  .sort(function (a, b) {
+    return a - b; // 오름차순 정렬 a>b
+  })
+  .join(" ");
+
+if (heightIn === sorted) {
+  console.log("YES");
+} else {
+  console.log("NO");
+}
+
+// .sort(function(a, b)  {
+//   if(a > b) return 1; // 오름차순
+//   if(a === b) return 0;
+//   if(a < b) return -1; // 내림차순
+// });
+
+// 오름차순 정렬 단순화 !*
+// .sort(function (a, b) {
+//   return a - b;
+// })
+
+// 니람차순 정렬 단순화 !*
+// .sort(function (a, b) {
+//   return b - a;
+// })
 
 // ** 다시풀기
 // 문제35 : Factory 함수 사용하기
@@ -110,12 +134,69 @@ console.log(gugudan);
 // 3) 변수에서 Math.max 구함
 // 4) 가장 많이 받은 이름(중복제거)과 Math.max로 콘솔 출력
 
-let candidate = console.log("학생들이 뽑은 후보 입력").split(" ");
+let candidate = prompt("학생들이 뽑은 후보 입력").split(" ");
+let result = {}; // candidate 담을 객체 선언
+let winner = ""; // 반장
 
-let name = {};
-
-for (let i = 0; i < candidate.length; i++) {
-  name = candidate[i];
+// for in : candidate 배열의 길이만큼 반복을 수행
+for (let index in candidate) {
+  let validation = candidate[index];
+  // result 객체에 키값에 해당하는 값이 있다면 +1을 하고, 만약 키값이 없다면(undefined) 키값에 1을 추가
+  result[validation] =
+    result[validation] === undefined
+      ? 1
+      : (result[validation] = result[validation] + 1);
 }
 
-//
+// 키값만 따로 빼는 Object.keys 메서드 사용
+// reduce 메서드 활용하여 result객체에 학생들의 이름을 넣었을 때 max값을 도출하는 return 문, 그 max값이 가장 큰 학생을 도출하는 로직 수행
+winner = Object.keys(result).reduce(function (a, b) {
+  return result[a] > result[b] ? a : b;
+});
+
+console.log(`${winner}(이)가 총 ${result[winner]}표로 반장이 되었습니다`);
+
+// ** 막힘 문제38 : 호준이의 아르바이트
+// 1위부터 3위를 한 아이들에게 상으로 사탕을 줄 것이다.
+// 1 ~ 3위 학생은 여러명일 수 있고 중복되는 학생까지 포함하여 사탕을 사기로 한다.
+// 학생들의 점수를 공백으로 구분하여 입력을 받고 사탕을 받을 학생의 수를 출력하라
+
+// 입력: 97 86 75 55 97 85 97 97 95
+// 출력 : 6
+
+// 1) sort - 내림차순
+// 2) 상위 3개, but 중복 포함
+// 3) 사탕받을 학생 수 변수 선언 및 생성
+
+let score = prompt("학생 점수 공백으로 모두 입력");
+let winTop3 = [];
+let count = 0;
+winTop3 = score.split(" ").sort(function (a, b) {
+  return b - a;
+});
+// 여기서 막힙!
+// 힌트 .map, while문, .pop(), includes(), .push() 사용!
+
+// 문제39 : 오타 수정하기
+// 문장이 입력되면 모든 q를 e로 바꾸는 프로그램 작성
+
+// 입력 : querty
+// 출력 : euerty
+
+// 입력 : hqllo my namq is hyqwon
+// 출력 : hello my name is hyewon
+
+// 1. 정규표현식
+let typo = prompt("문장 입력");
+console.log(typo.replace(/q/gi, "e"));
+
+// 2. replace
+let typo2 = prompt("문장 입력");
+console.log(typo2.replaceAll("q", "e"));
+
+// ** 다시 풀기! 문제40 : 놀이동산에 가자
+// 인원수 제한은 없으나 제한 무게를 넘으면 무조건 다음 기구를 타야합니다.
+
+// 원범이와 친구들이 총 몇 명 탈 수 있는지 알 수 있는 프로그램을 작성
+
+// 첫번째 입력으로 제한 무게가 주어지고 두번째 입력으로는 함께한 친구들의 수 n이 주어집니다. 그 다음 차례대로 탑승할 친구들의 몸무게가 주어집니다. 몸무게는 무작위로 주어집니다
