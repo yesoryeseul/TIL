c95308843433c415cdf5d0a339dfcdc0cb843fab
yesoryeseul <yesoryeseul07@gmail.com>
2023-03-30 23:55:48 +0900

    코드페스티벌 37번 진행중

--- 코드페스티벌/js/ch01_31_40re.js
+++ 코드페스티벌/js/ch01_31_40re.js
@@ -57,15 +57,14 @@ one(n) 에 n은 제곱값
 a(10)이 들어가는 과정이 이해가 안감
 -> 1) function one이 function two를 리턴하기 때문에 one은 결국 two함수
 Math.pow()
-일급객체 개념
+일급객체 개념, 클로저
 
 */
 
 function one(n) {
   function two(v) {
     //pass
-    const res = Math.pow(v, n);
-    return res;
+    return Math.pow(v, n);
   }
   return two; // ***
 }
@@ -88,3 +87,34 @@ console.log(b(10)); // 10*10*10의 결과가 나와야함
 console.log(c(10)); // 10*10*10*10의 결과가 나와야함
 
 // 3개가 다른함수
+
+
+// 문제37 : 반장 선거
+// 학생들이 뽑은 후보들을 입력받으면 뽑힌 학생의 이름과 받은 표 수를 출력하는 프로그램을 작성
+
+// 입력 : 사쿠라 사쿠라 사쿠라 사쿠라 채원 채원 윤진 윤진
+// 출력 : 사쿠라(이)가 총 4표로 반장이 되었습니다
+
+/* 결과
+return { 사쿠라: 4 }
+2. function leader
+1. 입력 prompt 사쿠라 사쿠라 사쿠라 사쿠라 채원 채원 윤진 윤진 하여 split(" ")
+2. 수를 count 하여 가장 많은 사람과 표 수를 return
+*/
+function leader() {
+  let leaderList = prompt("반장 후보를 투표하세요(입력방식)").split(" ");
+  // console.log(leaderList); // 입력 객체가 leaderList라는 배열 값에 담깁
+    
+  let count = [];
+  leaderList.forEach((x) => { 
+    count[x] = (count[x] || 0)+1; 
+  });
+
+  // console.log(count); // 입력 객체가 leaderList라는 배열 값에 담깁
+  
+  let banjang = Math.max(...count);
+  console.log(banjang);
+  // let leader = count;
+  // return { leader };
+}
+leader();
