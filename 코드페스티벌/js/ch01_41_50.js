4a9ee422d4dd9c54b632837811b459288d9e413e
yesoryeseul <yesoryeseul07@gmail.com>
2023-04-03 18:33:04 +0900

    코드페스티벌 47 set 자료형의 응용

--- 코드페스티벌/js/ch01_41_50.js
+++ 코드페스티벌/js/ch01_41_50.js
@@ -17,7 +17,7 @@
 
 let num = prompt("숫자를 입력하세요");
 
-function primeNumberisTrue() {
+function primeNumberisTrue(num) {
   for (let n = 2; n < num; n++) {
     const result = num % n;
     if (result === 0) {
@@ -33,7 +33,7 @@ function primeNumberisTrue() {
   console.log("YES");
 }
 
-primeNumberisTrue();
+primeNumberisTrue(11);
 
 /*
 # 문제42 : 2020년 ***
@@ -145,7 +145,46 @@ console.log(date.getFullYear());
 
 // 1. 배열에 1부터 20까지 답기
 let arr = [];
+let total = 0;
 for(let i = 0; i < 20; i++) {
   arr[i] = i+1;
 }
 // console.log(arr); // 디버깅 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+arr.forEach((n) => {
+  while(n !== 0){
+    total += (n % 10);
+    n = Math.floor(n/10);
+  }
+})
+
+console.log(total);
+
+/*
+# 문제47 : set 자료형의 응용
+
+바울랩에서는 3월 29일 제주대학교에서 '제주 빅데이터 사회혁신 해커톤' 행사를 주최하게 되었습니다. 이에 구글 설문지를 배포하였으나 제주대학생들이 중복해서 n개씩 설문지를 제출하였습니다. 
+**중복된 데이터들을 삭제하여 실제 접수 명단이 몇 명인지 알고 싶습니다.**
+
+아래 주어진 데이터들로부터 중복을 제거하여 **실제 접수 인원을 출력**해 주세요.
+*/
+
+const people = {
+  이호준: "01050442903",
+  이호상: "01051442904",
+  이준호: "01050342904",
+  이호준: "01050442903",
+  이준: "01050412904",
+  이호: "01050443904",
+  이호준: "01050442903"
+};
+
+// set 객체 선언
+const newSet = new Set();
+
+// 객체 for...in
+for(const candidate in people) {
+  newSet.add(people[candidate]); // set은 값을 추가하기 위해 .add 메서드사용
+}
+
+console.log(newSet.size); // 5, set은 중복을 제거하여 길이를 size로 표현
