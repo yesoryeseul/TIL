76e6d9db898715fc36be96e2891d8841b593c840
yesoryeseul <yesoryeseul07@gmail.com>
2023-04-07 00:50:19 +0900

    프로그래머스 x만큼 간격이 있는 n개의 숫자 re

--- 프로그래머스/lv1/05_x만큼간격이있는n개의숫자.js
+++ 프로그래머스/lv1/05_x만큼간격이있는n개의숫자.js
@@ -10,11 +10,6 @@ x	n	answer
 -4	2	[-4, -8]
 */
 
-/*
-x부터 시작하여 x씩 증가..
-2 + 2 = 4, 2 + 2 + 2 = 6,  x += x, x + x + x = 3x
-*/
-
 function solution(x, n) {
   let answer = []; // 담을 배열
   for(let i = 1; i <= n; i++) {
@@ -22,6 +17,24 @@ function solution(x, n) {
   }
   return answer;
 }
+
+// refactoring 1
+// n개를 넣을 수 있는 배열을 선언하여 x값으로 채워놓고
+// map 메서드 (인덱스+1)*x값
+// ex) solution(2, 5)
+// Array(n).fill(x) 여기까지의 결과는 [ 2, 2, 2, 2, 2 ]
+// 1*2, 2*2, 3*2, 4*2, 5*2
+function solution(x, n) {
+  return Array(n).fill(x).map((v, i) => (i + 1) * v);
+}
+
+// refactoring 2
+// [...Array(n).keys()] => n개의 배열이 인덱스의 값으로 채워짐
+// ex) solution(2, 5)의 경우 [ 0, 1, 2, 3, 4 ] 로 채운 상태
+function solution(x, n) {
+  return [...Array(n).keys()].map(v => (v + 1) * x);
+}
+
 console.log(solution(2, 5)) // [2, 4, 6, 8, 10]
 console.log(solution(4, 3)) // [4, 8, 12]
 console.log(solution(-4, 2)) // [-4, -8]
