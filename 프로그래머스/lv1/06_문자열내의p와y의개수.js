e7b30885a35e0536d21a0d99f6364fadbba6fb7e
yesoryeseul <yesoryeseul07@gmail.com>
2023-04-07 17:46:48 +0900

    프로그래머스 나머지가 1이 되는 수 찾기

--- 프로그래머스/lv1/06_문자열내의p와y의개수.js
+++ 프로그래머스/lv1/06_문자열내의p와y의개수.js
@@ -38,5 +38,11 @@ function solution(s){
   return s.match(/p/gi).length === s.match(/y/gi).length;
 }
 
+// refactoring
+// toUpperCase() 메서드 이용
+function solution(s){
+  return s.toUpperCase().split("P").length === s.toUpperCase().split("Y").length;
+}
+
 console.log(solution("pPoooyY")) // true
 console.log(solution("Pyy")) // false
