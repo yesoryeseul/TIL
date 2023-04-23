7e9d764d39097026aee56021f3920cee92804043
yesoryeseul <yesoryeseul07@gmail.com>
2023-04-23 20:32:19 +0900

    프로그래머스 문제풀이 코드 수정

--- 프로그래머스/기초/02공배수.js
+++ 프로그래머스/기초/02공배수.js
@@ -28,7 +28,7 @@ function solution(number, n, m) {
 }
 
 function solution(number, n, m) {
-  return number % n === 0 && number % m === 0 ? 1 : 0 : 0;
+  return (number % n === 0) ? (number % m === 0) ? 1 : 0 : 0;
 }
 
 console.log(solution(60, 2, 3))
