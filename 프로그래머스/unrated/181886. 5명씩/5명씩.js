a7ef75b422615864e3c345f1994b3af7613217c5
Kim Yeseul <123865139+yesoryeseul@users.noreply.github.com>
2023-11-24 22:17:50 +0900

    [level unrated] Title: 5명씩, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.js
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.js
@@ -1,7 +1,8 @@
 function solution(names) {
-    let arr = [];
-    for(let i = 0; i < names.length; i += 5) {
-        arr.push(names[i]);
-    }
-    return arr;
+    // let arr = [];
+    // for(let i = 0; i < names.length; i += 5) {
+    //     arr.push(names[i]);
+    // }
+    // return arr;
+    return names.filter((v, i) => !(i % 5));
 }
