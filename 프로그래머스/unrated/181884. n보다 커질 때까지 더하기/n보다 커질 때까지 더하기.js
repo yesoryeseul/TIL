fdcf4d1a24f2271b444d12e0dfa815c1ea91395f
Kim Yeseul <123865139+yesoryeseul@users.noreply.github.com>
2023-11-25 23:33:45 +0900

    [level unrated] Title: n보다 커질 때까지 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,12 @@
+function solution(numbers, n) {
+    let answer = 0;
+    for(let val of numbers) {
+        if(answer <= n) answer += val;
+    }
+    return answer;
+}
+
+/*
+1. for문 순회해서 answer에 numbers[i]를 누적연산한다.
+2. 조건은 answer <= n 일 때까지만 누적연산을 한다.
+*/
