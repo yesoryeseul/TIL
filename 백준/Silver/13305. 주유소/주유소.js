9d54f8eb9cbb4ad5bbd238e1d73b838a63c6ba99
Kim Yeseul <123865139+yesoryeseul@users.noreply.github.com>
2023-11-28 10:44:08 +0900

    [Silver III] Title: 주유소, Time: 284 ms, Memory: 35952 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -6,7 +6,7 @@ let n = Number(input[0]); // 반복 횟수
 let distance = input[1].split(' ').map(Number); // 거리 배열
 let cost = input[2].split(' ').map(Number); // 주유소 당 비용
 
-// [5, 2, 4, 1] => [5, 2, 2, 2] 로 바꾸는 것!
+// [5, 2, 4, 1] => [5, 2, 2, 1] 로 바꾸는 것!
 let first = cost[0];
 for(let i = 0; i < n; i++) {
   first = Math.min(first, cost[i])
@@ -15,8 +15,8 @@ for(let i = 0; i < n; i++) {
 
 // 도로당 이용합
 // [2, 3, 1] => 5 * 2 + 3 * 2 + 1 * 2
-let sum = 0;
+let sum = BigInt(0);
 for(let i = 0; i < n - 1; i++) {
-  sum += distance[i] * cost[i];
+  sum += BigInt(distance[i]) * BigInt(cost[i]);
 }
-console.log(sum);
+console.log(String(sum));
